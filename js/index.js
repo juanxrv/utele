58952de5ecbea04e8e28ffe6b282f92d0376dbbd
juanxrv <juanxxrvillanueva@gmail.com>
2023-10-29 12:39:56 -0600

    ola

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Inicializar el carrusel de noticias
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
